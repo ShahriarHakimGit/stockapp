36f223c8ac7d3a03758b1e1a67716795478ca2c0
Shahriar Hakim <shahriarhakim@Shahriars-MacBook-Pro.local>
2023-07-12 16:19:44 +0600

    almost donne

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ function Header() {
   return (
     <header className="flex justify-between items-center lg:px-20">
         <ul className="flex items-center justify-between">
-            <li className="ml-20 sm:ml-20 md:ml-2">
+            <li className="ml-20">
                 <Link to="/" className="no-underline text-emerald-500 hover:bg-slate-600 rounded-full p-2">Search</Link>
             </li>
-            <li className="ml-20 sm:ml-20 md:ml-2">
+            <li className="ml-20">
                 <Link to="/saved" className="no-underline text-emerald-500 hover:bg-slate-600 rounded-full p-2">Saved</Link>
             </li>
-            <li className="ml-20 sm:ml-20 md:ml-2">
+            <li className="ml-20">
                 <Link to="/about" className="no-underline text-emerald-500 hover:bg-slate-600 rounded-full p-2">About</Link>
             </li>
         </ul>
